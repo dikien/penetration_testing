ddd99b1ed1686268fb5ed52a147019fe63de3cb6
jongwon <dikien2012@gmail.com>
2014-02-16 15:26:38 +0900

    check ftp anonymouse

---
+++ ftp_anonymous.py
@@ -0,0 +1,14 @@
+from ftplib import FTP
+
+servers = file("21_ports_FTP.txt").readlines()
+
+for server in servers:
+    server = server.strip()
+    try:
+        print server
+        ftp = FTP(server)   # connect to host, default port
+        ftp.login()               # user anonymous, passwd anonymous@
+        ftp.retrlines('LIST')     # list directory contents
+    except Exception, e:
+        print e
+        pass
